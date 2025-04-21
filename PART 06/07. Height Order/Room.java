f0bd294c2742aaf1028f3950f3406c557b94fa1f
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-21 20:26:35 +0800

    Room.java

---
+++ PART 06/07. Height Order/Room.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+
+public class Room {
+    private ArrayList<Person> list;
+    
+    public Room(){
+        this.list = new ArrayList<>();
+    }
+    
+    public void add(Person person){
+        this.list.add(person);
+    }
+    
+    public boolean isEmpty(){
+        if(this.list.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+    
+    public ArrayList<Person> getPersons(){
+        return this.list;
+    }
+}
