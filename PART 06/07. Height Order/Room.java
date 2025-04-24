8d010c9aa3d3c041552e854051f952b8b23f74ae
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-24 22:18:45 +0800

    Room.java

--- PART 06/07. Height Order/Room.java
+++ PART 06/07. Height Order/Room.java
@@ -1,6 +1,6 @@
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class Room {
     private ArrayList<Person> list;
@@ -23,7 +23,28 @@ public class Room {
     public ArrayList<Person> getPersons(){
         return this.list;
     }
-
+    
     public Person shortest(){
+        if(this.list.isEmpty()) return null;
+        
+        Person returnObj = this.list.get(0);
+        
+        for(Person person: this.list){
+            if(person.getHeight() < returnObj.getHeight()) returnObj = person;
+        }
+        return returnObj;
+    }
+    
+    public Person take(){
+        if(this.list.isEmpty()) return null;
+        
+        Person returnObj = this.list.get(0);
+        
+        for(Person person: this.list){
+            if(person.getHeight() < returnObj.getHeight()) returnObj = person;
+        }
+        
+        //Collections.sort(list);
+        return returnObj;
     }
 }
