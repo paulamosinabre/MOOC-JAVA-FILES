2aa38d0545a39b2f425de11c6a968cc8f5e1ba9e
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-06-26 16:15:05 +0800

    Update Hold.java

--- PART 06/08. CargoHold/Hold.java
+++ PART 06/08. CargoHold/Hold.java
@@ -2,30 +2,36 @@
 import java.util.ArrayList;
 
 public class Hold {
-    
+
     private int maxWeight;
     private ArrayList<Suitcase> suitcases = new ArrayList<>();
-    
-    public Hold(int maxWeight){
-        this.maxWeight = maxWeight; 
-    }        
-    
-    public void addSuitcase(Suitcase suitcase){
-        if(suitcase.totalWeight() < maxWeight) {
+
+    public Hold(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public void addSuitcase(Suitcase suitcase) {
+        int currentTotalWeight = 0;
+        for (Suitcase s : suitcases) {
+            currentTotalWeight += s.totalWeight();
+        }
+
+        if (currentTotalWeight + suitcase.totalWeight() > maxWeight) {
             return;
         }
+
         suitcases.add(suitcase);
     }
-    
-    public void printItems(){
-        for(Suitcase suitcase: suitcases){
+
+    public void printItems() {
+        for (Suitcase suitcase : suitcases) {
             suitcase.printItems();
         }
     }
-    
-    public String toString(){
+
+    public String toString() {
         int totalWeight = 0;
-        for(Suitcase suitcase: suitcases){
+        for (Suitcase suitcase : suitcases) {
             totalWeight += suitcase.totalWeight();
         }
         return suitcases.size() + " suitcases (" + totalWeight + " kg)";
