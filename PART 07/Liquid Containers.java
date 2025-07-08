78d8dbaf14aad0f123ddc858dfee154c1fc42643
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-07-08 20:31:15 +0800

    Update Liquid Containers.java

--- PART 07/Liquid Containers.java
+++ PART 07/Liquid Containers.java
@@ -8,40 +8,50 @@ public class LiquidContainers {
 
         int first = 0;
         int second = 0;
-        
+
         while (true) {
 
             System.out.println("First: " + first + "/100");
-            System.out.println("Second: " + second + "/100"); 
+            System.out.println("Second: " + second + "/100");
             System.out.println("");
-            
+
             String input = scan.nextLine();
-            String[] parts = input.split(" ");
-            String cmd = parts[0];
-            int amount = Integer.valueOf(parts[1]);
+
             if (input.equals("quit")) {
                 break;
             }
 
-            if (cmd.equals("add")) {
-                int total = first + amount;
-               
-                first = total;
-                System.out.println(total);
-                 
-                if(total >= 100){
+            String[] parts = input.split(" ");
+            String cmd = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+
+            if (cmd.equals("add") && amount > 0) {
+
+                first += amount;
+
+                if (first >= 100) {
                     first = 100;
                 }
-            } else if (cmd.equals("move")){
-                int totalSec = second + amount;
-                second = totalSec;
-                first -= amount;
+            } else if (cmd.equals("move") && amount > 0) {
                 
-                if(totalSec >= 100){
+                if(amount > first){
+                    amount = first;
+                }
+                
+                first -= amount;
+                second += amount;
+                
+                if (second > 100) {
                     second = 100;
+                }
+            } else if (cmd.equals("remove") && amount > 0) {
+                second -= amount;
+
+                if (second < 0) {
+                    second = 0;
                 }
             }
         }
     }
-    
+
 }
