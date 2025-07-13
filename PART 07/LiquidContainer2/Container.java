bcd5d2cf80ba7e1ebf0d6b95b4f583a84f2d12b4
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-07-13 21:34:39 +0800

    Update Container.java

--- PART 07/LiquidContainer2/Container.java
+++ PART 07/LiquidContainer2/Container.java
@@ -11,11 +11,18 @@ public class Container {
         if (amount > 0) {
             this.liquid += amount;
         }
+        if (this.liquid > 100){
+            this.liquid = 100;
+        }
     }
     
     public void remove(int amount) {
         if (amount > 0) {
             this.liquid -= amount;
+        }
+        
+        if (this.liquid < 0){
+            this.liquid = 0;
         }
     }
 
